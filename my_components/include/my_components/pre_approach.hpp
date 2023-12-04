a79d6af73b618221089668c8fc144b82b2cc93e7
Naveen <therealnaveenkamal@gmail.com>
2023-12-04 11:26:44 +0000

    Linear and Angular Rotation Completed using Components

---
+++ my_components/include/my_components/pre_approach.hpp
@@ -0,0 +1,41 @@
+#ifndef COMPOSITION__PRE_APPROACH_HPP_
+#define COMPOSITION__PRE_APPROACH_HPP_
+
+#include "my_components/visibility_control.h"
+#include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include <chrono>
+#include <math.h>
+
+namespace my_components {
+
+class PreApproach : public rclcpp::Node {
+public:
+  COMPOSITION_PUBLIC
+  explicit PreApproach(const rclcpp::NodeOptions &options);
+
+private:
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_subscription_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher_;
+  int obs;
+  int deg;
+
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscription_;
+
+  bool is_rotating_ = false;
+  double initial_yaw_;
+  double target_yaw_;
+  double current_yaw_;
+
+protected:
+  void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+  void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+};
+
+}  // namespace composition
+
+#endif  // COMPOSITION__PRE_APPROACH_HPP_
---
+++ my_components/src/pre_approach.cpp
@@ -0,0 +1,89 @@
+#include "my_components/pre_approach.hpp"
+
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <utility>
+
+#include "geometry_msgs/msg/twist.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+namespace my_components {
+
+PreApproach::PreApproach(const rclcpp::NodeOptions &options)
+    : Node("pre_approach_node", options) {
+
+  scan_subscription_ = create_subscription<sensor_msgs::msg::LaserScan>(
+      "/scan", 10, [this](const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+        scan_callback(msg);
+      });
+
+  odom_subscription_ = create_subscription<nav_msgs::msg::Odometry>(
+      "/diffbot_base_controller/odom", 10,
+      [this](const nav_msgs::msg::Odometry::SharedPtr msg) {
+        odom_callback(msg);
+      });
+
+  cmd_vel_publisher_ = create_publisher<geometry_msgs::msg::Twist>(
+      "/diffbot_base_controller/cmd_vel_unstamped", 10);
+}
+
+void PreApproach::scan_callback(
+    const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+
+  double obs = 0.3;
+  int deg = -90;
+
+  RCLCPP_INFO(get_logger(), "Target: %f; Curr: %f", obs, msg->ranges[540]);
+
+  geometry_msgs::msg::Twist cmd_vel_msg;
+
+  if (!is_rotating_) {
+    if (std::abs(msg->ranges[540] - obs) < 0.05) {
+      cmd_vel_msg.linear.x = 0.0;
+      is_rotating_ = true;
+    } else {
+      cmd_vel_msg.linear.x = 0.5;
+      cmd_vel_msg.angular.z = 0.0;
+    }
+  }
+
+  target_yaw_ = deg * M_PI / 180.0;
+
+  if (is_rotating_) {
+    RCLCPP_INFO(get_logger(), "Curr: %f; Targ: %f; Err: %f", current_yaw_,
+                target_yaw_, std::abs(current_yaw_ - target_yaw_));
+
+    initial_yaw_ = current_yaw_;
+
+    if (std::abs(current_yaw_ - target_yaw_) > 0.05) {
+      cmd_vel_msg.angular.z = -0.2;
+      cmd_vel_msg.linear.x = 0.0;
+      is_rotating_ = true;
+    } else {
+      cmd_vel_msg.angular.z = 0.0;
+      cmd_vel_msg.linear.x = 0.0;
+      cmd_vel_publisher_->publish(cmd_vel_msg);
+      rclcpp::shutdown();
+    }
+  }
+
+  cmd_vel_publisher_->publish(cmd_vel_msg);
+}
+
+void PreApproach::odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) {
+  tf2::Quaternion quat;
+  tf2::fromMsg(msg->pose.pose.orientation, quat);
+  tf2::Matrix3x3 mat(quat);
+  double roll, pitch, yaw;
+  mat.getRPY(roll, pitch, yaw);
+  current_yaw_ = yaw;
+}
+
+} // namespace my_components
+
+#include "rclcpp_components/register_node_macro.hpp"
+RCLCPP_COMPONENTS_REGISTER_NODE(my_components::PreApproach)
